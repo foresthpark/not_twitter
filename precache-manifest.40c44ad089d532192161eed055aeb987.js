c2852c3ec936b11017b439350a85d41d8e210a60
Forest Park <foresthpark@gmail.com>
2020-09-16 11:39:35 -0600

    Updates

---
+++ precache-manifest.40c44ad089d532192161eed055aeb987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5274e567ece33c05c881aa68780f10",
+    "url": "/not_twitter/index.html"
+  },
+  {
+    "revision": "98bdb90fc5d34fae4944",
+    "url": "/not_twitter/static/css/main.ef091cd1.chunk.css"
+  },
+  {
+    "revision": "d72ddddcbbfa8f74c1ad",
+    "url": "/not_twitter/static/js/2.3e79d280.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/not_twitter/static/js/2.3e79d280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98bdb90fc5d34fae4944",
+    "url": "/not_twitter/static/js/main.b7c57d00.chunk.js"
+  },
+  {
+    "revision": "6b1f60c3ceb2c94f78ba",
+    "url": "/not_twitter/static/js/runtime-main.e1f6f0f8.js"
+  }
+]);
